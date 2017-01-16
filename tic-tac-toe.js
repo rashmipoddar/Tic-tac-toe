0565f01cccdd272117a8cd3928dc800a8f972707
Rashmi <poddar.rashmi@gmail.com>
2017-01-16 15:31:09 -0500

    added puedocode

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,18 @@
-var board = 
+prompt = require('prompt');
+
+// var board = ' _ _ _\n _ _ _\n _ _ _';
+// console.log(board);
+
+prompt.start();
+prompt.get(['player1Name', 'player2Name'], function(err, result) {
+  console.log("The first player is ", result.player1Name);
+  console.log("The second player is ", result.player2Name);
+})
+
+var board = [['_ ', '_ ', '_ '], ['_ ', '_ ', '_ '], ['_ ', '_ ', '_ ']];
+
+
+
+// Initialize an empty board to be a 2d array
+// Ask the players to provide an input for each turn indicating both the value and its position on the board.
+//
